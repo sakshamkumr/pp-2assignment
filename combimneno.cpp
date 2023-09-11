2567237c8f6c527aa5fa9c5bf2ae57bfd844813d
sakshamkumr <122541184+sakshamkumr@users.noreply.github.com>
2023-09-11 23:06:22 +0530

    Add files via upload

---
+++ combimneno.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int n;
+cin >> n;
+for(int i = 0; i < n; ++i) {
+cout << i+1 << " ";
+}
+for(int i = n-1; i >= 1; --i) {
+cout << i << " ";
+}
+cout << endl;
+for(int i = 1; i < n; ++i) {
+for(int j = 0; j < n - i; ++j) {
+cout << j+1 << " ";
+}
+for(int j = 0; j < 2*i-1; ++j) {
+cout << " ";
+}
+}}
---
+++ invertedV.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int n;
+cin >> n;
+// Printing the topmost star: 0th row
+for(int i = 0; i < n; ++i)
+cout << " ";
+cout << "*" << endl;
+// Printing the rest of the upper triangle: [1...n] rows
+for(int i = 1; i <= n; ++i) {
+// Printing initial spaces
+for(int j = 0; j < n - i; ++j) {
+cout << " ";
+}
+cout << "*";
+int m = 2 * i - 1;
+// Printing middle spaces
+for(int j = 0; j < m; ++j) {
+cout << " ";}}}
